5fdea572a5a6fa5581379b018fd857de869b3af9
UniProject <shakedkod@gmail.com>
2024-11-05 21:30:55 +0200

    lesson 2 done

--- mmn11/q1/letters.c
+++ mmn11/q1/letters.c
@@ -1,29 +1,18 @@
 #include <stdio.h>
 #include <ctype.h>
 
-int isSpace(const char c)
-{
-    return ((c == ' ') || (c == '\t') || (c == '\n'));
-}
-
-int isNumber(const char c)
-{
-    return (c >= '0') && (c <= '9');
-}
-
 int main()
 {
-    char c;
-    int isStartOfSentence = 1, isQuote = 0;
+    char c, isStartOfSentence = 1, isQuote = 0;
 
     printf("Hello, please enter some sentences into the machine:\n");
     while ((c = getchar()) != EOF)
     {
-        if (!isNumber(c))
+        if (!isdigit(c))
         {
             if (isQuote) /* in quote */
                 c = toupper(c);
-            else if (isStartOfSentence && !isSpace(c)) /* first letter of a sentence */
+            else if (isStartOfSentence && !isspace(c)) /* first letter of a sentence */
             {
                 c = toupper(c);
                 isStartOfSentence = 0;
@@ -38,7 +27,7 @@ int main()
                 isStartOfSentence = 1;
 
             /* output */
-            putchar(c);
+            printf("%c", c);
         }
     }
 
