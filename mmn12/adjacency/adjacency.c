7882944ea5bbcc5aee66a5d628064d21b455ca98
UniProject <shakedkod@gmail.com>
2024-12-10 17:19:25 +0200

    path is done but is not checked

--- mmn12/adjacency/adjacency.c
+++ mmn12/adjacency/adjacency.c
@@ -1,3 +1,24 @@
+#include "constants.h"
+
+Bool path(adjmat treeMap, int indexU, int indexV)
+{
+    int i = 0;
+
+    if (indexU >= N || indexV >= N)
+        return FALSE;
+    if (treeMap[indexU][indexV])
+        return TRUE;
+    
+    for (; i < N; i++)
+    {
+        if (treeMap[indexU][i])
+            if (path(treeMap, i, indexV))
+                return TRUE;
+    }
+
+    return FALSE;
+}
+
 int main()
 {
     return 0;
---
+++ mmn12/adjacency/constants.h
@@ -0,0 +1,13 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+typedef enum 
+{
+    FALSE,
+    TRUE
+} Bool;
+
+#define N 10
+typedef Bool adjmat[N][N];
+
+#endif
