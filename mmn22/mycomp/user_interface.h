e1669668eb71d059c4a90117fb9a20ad72ae8fe5
UniProject <shakedkod@gmail.com>
2025-01-17 19:43:05 +0200

    V2

--- mmn22/mycomp/user_interface.h
+++ mmn22/mycomp/user_interface.h
@@ -3,6 +3,16 @@
 
 #include "coms.h"
 
+/* 
+    a functions that read's a line from stdin and execute the command that has been given according to a set of rules and guidelines
+
+    input:
+        vars variables: pointers to the six variables that are used in the commands.
+        boolean *is_eof: pointer to a boolean that is changed to TRUE if the line ends in EOF.
+    
+    output(code):
+        Error notice code or approval code.
+*/
 code execute_line(vars variables, boolean *is_eof);
 
 #endif
