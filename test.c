d5b1260ef80cc318728516d4f7751b194d322c3e
UniProject <shakedkod@gmail.com>
2025-02-20 15:33:37 +0200

    after the test

--- test.c
+++ test.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <ctype.h>
 
-typedef enum 
-{
-    false,
-    true
-} boolean;
+const double standard_rating = 10.0;
+double rating;
+int rate_the_ship(double, double, int, double);
 
 int main()
 {
-    
+    rate_the_ship(64.0, 45.3, 3, standard_rating);
+    printf("%g\n", rating);
+}
+
+int rate_the_ship(double lwl, double sailarea, int nsails, double rating)
+{
+    rating *= (3.2/lwl) * sailarea * .025 * nsails;
+    return 0;
+}
+
+/* MAGNIV */
+void replace(int *i, int *j)
+{
+    (*i) ^= (*j);
+    (*j) ^= (*i);
+    (*i) ^= (*j); 
 }
